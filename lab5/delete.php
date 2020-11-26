cacd3c2ae316909fe8a9dcba87a397d97d13102a
sunshiny2 <58939532+sunshiny2@users.noreply.github.com>
2020-11-26 21:44:02 +0500

    Update delete.php

---
+++ lab5/delete.php
@@ -0,0 +1,22 @@
+<?require_once 'engine/page/title.php';?>
+<?require_once 'engine/connection/connectionStart.php';?>
+<html>
+    <body>
+		<?
+			if(isset($_GET['id'])&&isset($_GET['query'])){
+                $id = htmlentities(mysqli_real_escape_string($link, $_GET['id']));
+                $index = htmlentities(mysqli_real_escape_string($link, $_GET['query']));
+                if($index=="house_info"){
+                    $index="house";
+                }
+                if($index=="debtor_info"){
+                    $index="debtor";
+                }
+                $query = "DELETE FROM $database.$index WHERE id='$id'";
+                $result = mysqli_query($link, $query) or die ("Ошибка в запросе");
+                header("Location: index.php");
+			}
+		?>
+	</body>
+</html>
+<?require_once 'engine/connection/connectionEnd.php';?>
