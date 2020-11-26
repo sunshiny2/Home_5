40710d023586f1eed7666bd5d51347cc05a79e69
sunshiny2 <58939532+sunshiny2@users.noreply.github.com>
2020-11-26 21:40:18 +0500

    Add engine & library project

---
+++ lab5/engine/connection/connection.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user = 'f0477125_user';
+$password = 'root';
+$database = 'f0477125_db';
+?>
---
+++ lab5/engine/page/title.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+	<head>
+        <?require_once 'head.php';?>
+        <title>
+            Александра Ярулина ЛР№5
+        </title>
+	</head>
+</html>
